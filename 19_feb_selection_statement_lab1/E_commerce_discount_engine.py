b6ba65ea9a66fbc7c58e0c27f6a51e4d6bcfc15b
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-21 11:54:41 +0530

    Create E_commerce_discount_engine.py

---
+++ 19_feb_selection_statement_lab1/E_commerce_discount_engine.py
@@ -0,0 +1,18 @@
+cart_value = float(input("Enter cart value: "))
+membership = input("Enter membership (Silver/Gold/Platinum): ").lower()
+festival = input("Is it festival season? (yes/no): ").lower()
+
+discount = 0
+
+if membership == "silver":
+    discount = 5
+elif membership == "gold":
+    discount = 10
+elif membership == "platinum":
+    discount = 15
+
+if festival == "yes":
+    discount = max(discount, 20)
+
+final_amount = cart_value - (cart_value * discount / 100)
+print("Final payable amount: ₹", final_amount)
