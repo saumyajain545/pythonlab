b8999b4f9e0a2ef6315c9160ee17934350ce1cf7
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-21 11:21:08 +0530

    Create class_dis.py

---
+++ 18_feb_conditional_statement/class_dis.py
@@ -0,0 +1,4 @@
+total_students = 125
+each_class = 30
+print("full classes can ce formend are:",(total_students//each_class))
+print("remaining students without class are",(total_students%each_class))
