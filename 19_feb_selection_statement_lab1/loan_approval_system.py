3dc117ed9f494cbb23f9f9064803b910df1f73a1
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-21 11:57:58 +0530

    Create loan_approval_system.py

---
+++ 19_feb_selection_statement_lab1/loan_approval_system.py
@@ -0,0 +1,13 @@
+credit = int(input("Enter credit score: "))
+income = float(input("Enter monthly income: "))
+existing_loan = float(input("Enter existing loan amount: "))
+
+if credit < 600:
+    print("Loan Rejected.")
+elif 600 <= credit <= 750:
+    if income < 30000 and existing_loan > 500000:
+        print("Loan Rejected due to low income and high existing loan.")
+    else:
+        print("Further income verification required.")
+else:
+    print("Loan Approved.")
