36b14ab81f7b3a558a1f1803f7b56d19e02be210
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-22 16:37:01 +0530

    Create plus_pattern.py

---
+++ 20_feb_self_learning/plus_pattern.py
@@ -0,0 +1,15 @@
+n = 5
+for i in range(n):
+    for j in range(n):
+        if i == n//2 or j == n//2:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
+
+#output
+#  *  
+#  *  
+#*****
+#  *  
+#  *  
