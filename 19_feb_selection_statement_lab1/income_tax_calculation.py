6a553501e067b83e039c6ca6f36138ad683ccd5e
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-22 14:59:31 +0530

    Update income_tax_calculation.py

--- 19_feb_selection_statement_lab1/income_tax_calculation.py
+++ 19_feb_selection_statement_lab1/income_tax_calculation.py
@@ -14,3 +14,8 @@ else:
     tax = (250000 * 0.05) + (500000 * 0.20) + (income - 1000000) * 0.30
 
 print("Total tax to pay: ₹", tax)
+
+#output
+#Enter annual income: 280000
+#Enter age: 65
+#Total tax to pay: ₹ 0
