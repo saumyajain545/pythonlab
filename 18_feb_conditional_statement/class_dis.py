5f5997ee9d239229907b188443ef3025220df983
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-22 14:20:02 +0530

    Update class_dis.py

--- 18_feb_conditional_statement/class_dis.py
+++ 18_feb_conditional_statement/class_dis.py
@@ -2,3 +2,6 @@ total_students = 125
 each_class = 30
 print("full classes can ce formend are:",(total_students//each_class))
 print("remaining students without class are",(total_students%each_class))
+#output
+#full classes can ce formend are: 4
+#remaining students without class are 5
