20aa62af2b3b9c45a6bd9d81ff2df8c554ce507c
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-23 20:40:25 +0530

    Create set_in_python.py

---
+++ 23_feb_class_work/set_in_python.py
@@ -0,0 +1,17 @@
+#set in python
+set1 = {45, 67, 89, 45, 67, 89, 34, 90, 45, 59, 120, 34, 90, 45, 59, 120}
+
+print("Set is : ")
+print(set1)
+
+# to insert 34 in set
+set1.add(34)
+
+print("After inserting 34 in set : ")
+print(set1)
+
+#output
+#Set is : 
+#{34, 67, 120, 89, 90, 59, 45}
+#After inserting 34 in set : 
+#{34, 67, 120, 89, 90, 59, 45}
