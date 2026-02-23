ecf81142e32097a01b70f9dcfd9a8d32aa2f7ff6
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-23 10:40:00 +0530

    Create swap_two_numbers.py

---
+++ 19_feb_self_learning_arithmetic_operator/swap_two_numbers.py
@@ -0,0 +1,16 @@
+# Assign values
+a = 5
+b = 9
+
+# Swap using arithmetic operators
+a = a + b
+b = a - b
+a = a - b
+
+# Print swapped values
+print("a:", a)
+print("b:", b)
+
+#output
+#a: 9
+#b: 5
