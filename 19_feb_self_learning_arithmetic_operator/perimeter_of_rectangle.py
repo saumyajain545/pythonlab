7416722966cdd126bdf11f27cfb6e6f6e6d560f9
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-23 10:28:08 +0530

    Create perimeter_of_rectangle.py

---
+++ 19_feb_self_learning_arithmetic_operator/perimeter_of_rectangle.py
@@ -0,0 +1,12 @@
+# Assign length and width
+length = 10
+width = 4
+
+# Calculate perimeter
+perimeter = 2 * (length + width)
+
+# Print perimeter
+print("Perimeter:", perimeter)
+
+#output
+#Perimeter: 28
