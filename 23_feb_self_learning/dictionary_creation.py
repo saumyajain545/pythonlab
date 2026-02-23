171b94ff4029b6d0b5c83c4250dac64c48b4d38d
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-23 21:21:06 +0530

    Create dictionary_creation.py

---
+++ 23_feb_self_learning/dictionary_creation.py
@@ -0,0 +1,10 @@
+# Dictionary
+student = {"name": "Amit", "age": 20, "course": "BCA"}
+
+# Printing keys and values
+print("Keys:", student.keys())
+print("Values:", student.values())
+
+#output
+#Keys: dict_keys(['name', 'age', 'course'])
+#Values: dict_values(['Amit', 20, 'BCA'])
