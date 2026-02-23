3329b2acc0a31eba2853b1388d3c96bbdeee681c
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-23 10:35:07 +0530

    Create simple_interest.py

---
+++ 19_feb_self_learning_arithmetic_operator/simple_interest.py
@@ -0,0 +1,13 @@
+# Assign values
+principal = 1000
+rate = 5
+time = 2
+
+# Calculate simple interest
+simple_interest = (principal * rate * time) / 100
+
+# Print result
+print("Simple Interest:", simple_interest)
+
+#output
+#Simple Interest: 100.0
