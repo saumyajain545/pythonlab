a5c0c51e28771002798fc79a8f9d59ccfca077ab
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-23 10:37:43 +0530

    Create convert_minutes_to_hours.py

---
+++ 19_feb_self_learning_arithmetic_operator/convert_minutes_to_hours.py
@@ -0,0 +1,16 @@
+# Assign total minutes
+minutes = 150
+
+# Convert to hours using floor division
+hours = minutes // 60
+
+# Find remaining minutes using modulus
+remaining_minutes = minutes % 60
+
+# Print results
+print("Hours:", hours)
+print("Remaining Minutes:", remaining_minutes)
+
+#output
+#Hours: 2
+#Remaining Minutes: 30
