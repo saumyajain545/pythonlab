a35bf61ddf96f0388b45a8aedf5b5404aa37abc5
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-23 10:33:46 +0530

    Create area_of_circle.py

---
+++ 19_feb_self_learning_arithmetic_operator/area_of_circle.py
@@ -0,0 +1,14 @@
+# Assign radius
+radius = 7
+
+# Assign value of pi
+pi = 3.14
+
+# Calculate area using formula πr²
+area = pi * radius ** 2
+
+# Print area
+print("Area of Circle:", area)
+
+#output
+#Area of Circle: 153.86
