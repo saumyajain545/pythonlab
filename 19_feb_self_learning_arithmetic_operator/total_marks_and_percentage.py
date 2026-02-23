884e10ed967e03096c17f92446488f94b644b768
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-23 10:43:12 +0530

    Create total_marks_and_percentage.py

---
+++ 19_feb_self_learning_arithmetic_operator/total_marks_and_percentage.py
@@ -0,0 +1,20 @@
+# Assign marks
+m1 = 80
+m2 = 75
+m3 = 90
+m4 = 85
+m5 = 70
+
+# Calculate total
+total = m1 + m2 + m3 + m4 + m5
+
+# Calculate percentage
+percentage = total / 5
+
+# Print results
+print("Total Marks:", total)
+print("Percentage:", percentage)
+
+#output
+#Total Marks: 400
+#Percentage: 80.0
