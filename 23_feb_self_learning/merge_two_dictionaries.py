122a473bfe4b623dd8835c439d43b2961992f912
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-23 21:35:33 +0530

    Create merge_two_dictionaries.py

---
+++ 23_feb_self_learning/merge_two_dictionaries.py
@@ -0,0 +1,11 @@
+# Two dictionaries
+d1 = {"a": 1, "b": 2}
+d2 = {"c": 3, "d": 4}
+
+# Merging dictionaries
+d1.update(d2)
+
+print("Merged dictionary:", d1)
+
+#output
+#Merged dictionary: {'a': 1, 'b': 2, 'c': 3, 'd': 4}
