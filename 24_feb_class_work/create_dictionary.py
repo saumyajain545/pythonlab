2b1359dc4a6d2aee76ea699038b9bcaae4d27e74
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-24 15:20:17 +0530

    Create create_dictionary.py

---
+++ 24_feb_class_work/create_dictionary.py
@@ -0,0 +1,9 @@
+# Program to create a dictionary and display its items
+
+student = {"name": "Rahul", "age": 20, "course": "BCA"}   # Create a dictionary
+
+for key in student:                                        # Loop through dictionary keys
+    print(key, ":", student[key])                          # Print key and corresponding value
+
+# Output Example
+# name : Rahul
