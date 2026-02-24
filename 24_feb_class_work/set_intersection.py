95ebd02e080dd0fb43efb312be72797080be8836
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-24 15:17:37 +0530

    Create set_intersection.py

---
+++ 24_feb_class_work/set_intersection.py
@@ -0,0 +1,11 @@
+# Program to perform intersection of two sets
+
+set1 = {1, 2, 3, 4}                 # Create first set
+set2 = {3, 4, 5, 6}                 # Create second set
+
+result = set1.intersection(set2)    # Perform intersection operation
+
+print("Intersection of sets:", result)  # Display intersection result
+
+# Output Example
+# Intersection of sets: {3, 4}
