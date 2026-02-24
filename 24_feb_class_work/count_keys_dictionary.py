49d7a5f38b4a24e0d8f4957f4cecac343c171a73
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-24 15:21:10 +0530

    Create count_keys_dictionary.py

---
+++ 24_feb_class_work/count_keys_dictionary.py
@@ -0,0 +1,10 @@
+# Program to count number of keys in a dictionary
+
+student = {"name": "Rahul", "age": 20, "course": "BCA"}   # Create a dictionary
+
+count = len(student)                                      # Count total keys using len()
+
+print("Number of keys:", count)                           # Display number of keys
+
+# Output Example
+# Number of keys: 3
