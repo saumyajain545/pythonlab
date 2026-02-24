e2238ebce3e571d4f4073d6c8806aaaaeb7c3a8a
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-24 10:29:01 +0530

    Add number classification script for input values

---
+++ 24_feb_class_work/check_negative_positive_and_zero.py
@@ -0,0 +1,18 @@
+#check whether a number is negative , positive or zero
+num=input(print("enter any number:"))
+#check for negative number
+if num<0:
+    print("negative number")
+#check for positive number
+elif num>0:
+    print("positive number")
+#check for zero
+elif num==0:
+    print("zero")
+#check for any invalid value
+else:
+    print("invalid input")
+
+#output
+#enter any number:-3
+#negative number
