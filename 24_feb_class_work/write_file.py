c9f9d81f7456f0d02c8650be6c0b9b88d2cda9ee
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-24 15:28:32 +0530

    Create write_file.py

---
+++ 24_feb_class_work/write_file.py
@@ -0,0 +1,10 @@
+# Program to write text into a file
+
+file = open("sample.txt", "w")        # Open file in write mode
+file.write("Hello Python")            # Write text into file
+file.close()                          # Close the file
+
+print("Data written successfully")    # Display success message
+
+# Output Example
+# Data written successfully
