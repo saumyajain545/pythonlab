05bf7c9eb1b664cb2b033a43a51ee15b92d8905f
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-24 15:17:00 +0530

    Create set_union.py

---
+++ 24_feb_class_work/set_union.py
@@ -0,0 +1,11 @@
+# Program to perform union of two sets
+
+set1 = {1, 2, 3, 4}                 # Create first set
+set2 = {3, 4, 5, 6}                 # Create second set
+
+result = set1.union(set2)           # Perform union operation
+
+print("Union of sets:", result)     # Display union result
+
+# Output Example
+# Union of sets: {1, 2, 3, 4, 5, 6}
