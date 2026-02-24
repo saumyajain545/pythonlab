1873ad92abf3a2eb712a21b7fa0a9e80d0a828ee
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-24 15:15:09 +0530

    Create tuple_to_list.py

---
+++ 24_feb_class_work/tuple_to_list.py
@@ -0,0 +1,10 @@
+# Program to convert tuple to list
+
+numbers = (1, 2, 3, 4, 5)             # Create a tuple
+
+converted_list = list(numbers)        # Convert tuple to list using list() function
+
+print("Converted list:", converted_list)   # Display converted list
+
+# Output Example
+# Converted list: [1, 2, 3, 4, 5]
