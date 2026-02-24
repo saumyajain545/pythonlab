565d8e43a1af610c6404bb5ef3a15fd87b8cf92c
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-24 11:35:55 +0530

    Create check_for_vowel_and_consonent.py

---
+++ 24_feb_class_work/check_for_vowel_and_consonent.py
@@ -0,0 +1,12 @@
+#check for vowels and consonents
+ch=input(print("enter any character:"))
+#check for vowel
+if ch=="a" or ch=="e" or ch=="i" or ch=="o" or ch=="u":
+    print("it is vowel")
+#it is consonent
+else:
+    print("it is consonents")
+
+#output
+#enter any character:a
+#it is vowel
