95fea8c9acb0af455a8f72d7f09644ec553e0c5e
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-24 15:18:14 +0530

    Create set_difference.py

---
+++ 24_feb_class_work/set_difference.py
@@ -0,0 +1,11 @@
+# Program to perform difference of two sets
+
+set1 = {1, 2, 3, 4}                 # Create first set
+set2 = {3, 4, 5, 6}                 # Create second set
+
+result = set1.difference(set2)      # Find elements present in set1 but not in set2
+
+print("Difference of sets:", result) # Display difference result
+
+# Output Example
+# Difference of sets: {1, 2}
