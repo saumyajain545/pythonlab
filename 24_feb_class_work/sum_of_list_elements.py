1a129a1ff3d90cf221f73fb10e25c6b9f315d0f7
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-24 15:06:39 +0530

    Create sum_of_list_elements.py

---
+++ 24_feb_class_work/sum_of_list_elements.py
@@ -0,0 +1,13 @@
+# Program to find the sum of elements in a list
+
+numbers = [10, 20, 30, 40]               # Create a list of numbers
+
+total = 0                                # Initialize sum variable
+
+for num in numbers:                      # Loop through each element
+    total = total + num                  # Add element to total sum
+
+print("Sum of list elements:", total)    # Display total sum
+
+# Output Example
+# Sum of list elements: 100
