ea4ab9b6dcaef12535e2cbb685cce8de3cadff42
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-24 15:18:50 +0530

    Create subset_check.py

---
+++ 24_feb_class_work/subset_check.py
@@ -0,0 +1,12 @@
+# Program to check whether one set is subset of another
+
+set1 = {1, 2}                       # Create first set
+set2 = {1, 2, 3, 4}                 # Create second set
+
+if set1.issubset(set2):             # Check if set1 is subset of set2
+    print("set1 is subset of set2") # Display subset result
+else:                               # Otherwise
+    print("set1 is not subset of set2")
+
+# Output Example
+# set1 is subset of set2
