621a3c88ee1c91e9f2a6deee864b8a4c1ff0fb16
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-24 12:14:56 +0530

    Create print_from_1_to_n.py

---
+++ 24_feb_class_work/print_from_1_to_n.py
@@ -0,0 +1,18 @@
+# Read the value of N from standard input
+N = int(input("Enter the value of N: "))
+
+# Initialize the counter variable
+i = 1
+
+# While loop to print numbers from 1 to N
+while i <= N:
+    print(i)
+    i = i + 1   # Increment the counter
+
+#output
+#Enter the value of N: 5
+#1
+#2
+#3
+#4
+#5
