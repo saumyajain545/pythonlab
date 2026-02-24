6abc2e27809f42a3d5c70e186639e1d012f3a4b1
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-24 10:44:12 +0530

    Create simple_interest.py

---
+++ 24_feb_class_work/simple_interest.py
@@ -0,0 +1,14 @@
+#to calculate simple interest
+rate=input(print("enter the rate of interest:"))
+principle=input(print("enter the principle amount:"))
+time=input(print("enter time in years:"))
+#calculating simple interest
+Simple_interest=(rate*principle*time)/100
+#displaying simple interest
+print("the simple interest is:",Simple_interest)
+
+#output
+#enter the rate of interest:3
+#enter the principle amount:2000
+#enter time in years:2
+#the simple interest is:120
