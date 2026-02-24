aa62b19d538185f602fe830f50eb97777dfcddf1
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-02-24 15:36:59 +0530

    Create lambda_function.py

---
+++ 24_feb_class_work/lambda_function.py
@@ -0,0 +1,11 @@
+# Program to demonstrate lambda function
+
+square = lambda x: x * x                # Create lambda function
+
+num = int(input("Enter a number: "))    # Take number input
+
+print("Square:", square(num))           # Call lambda function
+
+# Output Example
+# Enter a number: 5
+# Square: 25
