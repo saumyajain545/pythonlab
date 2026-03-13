2d75ceeb9d0a79205415ab0025c5fc606d9f849e
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-03-13 16:12:15 +0530

    Create numpy_operation.py

---
+++ 13_march_class_work/numpy_operation.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+number = np.array([30,40,10,20,50])
+
+# displaying the array
+print(number)
+
+# multiplying each element by 10
+number = number * 10
+
+# displaying the result
+print(number)
+
+#output
+#[30 40 10 20 50]
+#[300 400 100 200 500]
