f07801f9f9901d9e5d980124134dad6ad1e5bd1b
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-03-13 16:24:21 +0530

    Create statistical_functions.py

---
+++ 13_march_class_work/statistical_functions.py
@@ -0,0 +1,15 @@
+import numpy as np
+array1=np.array([32,56,78,90,45,89,12,13])
+print("first array")
+print(array1)
+average = np.mean(array1)
+print(average)
+print("standard deviation : " , np.std(array1))
+
+'''
+output
+first array
+[32 56 78 90 45 89 12 13]
+51.875
+standard deviation :  29.7381804251706
+'''
