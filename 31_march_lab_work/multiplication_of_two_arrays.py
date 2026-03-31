f52cad2ac05f3ba7a780b71a996921a6eec8b9b4
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-03-31 10:56:42 +0530

    element-wise multiplication of arrays

---
+++ 31_march_lab_work/multiplication_of_two_arrays.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Input arrays
+arr1 = np.array([1, 2, 3, 4])
+arr2 = np.array([5, 6, 7, 8])
+
+# Element-wise multiplication
+result = arr1 * arr2  # or np.multiply(arr1, arr2)
+
+# Output
+print("Resultant Array:", result)
+
+#output
+#Resultant Array: [ 5 12 21 32]
