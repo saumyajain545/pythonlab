fd1d7c7bd91cf5ad69a87e9266f8e866b6030871
saumyajain545 <saumya.jain.mca.2025@miet.ac.in>
2026-03-31 10:54:23 +0530

    mean, median, and std deviation of array

---
+++ 31_march_lab_work/mean_median_and_standard_deviation_of_array.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+# Input NumPy array
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+# Mean
+mean = np.mean(arr)
+
+# Median
+median = np.median(arr)
+
+# Standard Deviation
+std_dev = np.std(arr)
+
+# Output
+print("Mean:", mean)
+print("Median:", median)
+print("Standard Deviation:", std_dev)
+
+#output
+#Mean: 5.0
+#Median: 5.0
+#Standard Deviation: 2.581988897471611
